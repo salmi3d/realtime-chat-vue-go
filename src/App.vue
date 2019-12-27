d0b673456ab8d25ffb9ab4d5074affab5f95774a
salmi3d <salmi3d@gmail.com>
2019-12-27 15:37:02 +0200

    added chat component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
     <Header />
+    <Chat :messages="messages" />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Chat from './components/Chat.vue'
 
 export default {
   name: 'app',
@@ -16,19 +18,7 @@ export default {
   },
   components: {
     Header,
+    Chat,
   },
 }
 </script>
-
-<style lang="scss">
-body {
-  margin: 0;
-  padding: 0;
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-</style>
---
+++ src/components/Chat.vue
@@ -0,0 +1,13 @@
+<template functional>
+  <div class="chat">
+    <div class="chat__message" v-for="(message, index) in messages" :key="index">{{ message.body }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    messages: Array
+  }
+}
+</script>
