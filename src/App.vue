56c2de5fd42e140fda945d8ece568bc9cf8b3323
salmi3d <salmi3d@gmail.com>
2019-12-27 15:27:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      messages: []
+    }
+  },
+  components: {
+    Header,
+  },
+}
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,6 @@
+<template functional>
+  <div class="header">
+    <h1 class=header__title>Realtime Chat VUE & GO</h1>
+    <img width="50" alt="Vue logo" src="@/assets/logo.png">
+  </div>
+</template>
