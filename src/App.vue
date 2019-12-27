98e943b8aa9e3e59dfc8d7b6f3c01d2b93071e7c
salmi3d <salmi3d@gmail.com>
2019-12-27 15:49:47 +0200

    added input-box component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <div id="app">
     <Header />
     <Chat :messages="messages" />
+    <InputBox :send="send" />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
 import Chat from './components/Chat.vue'
+import InputBox from './components/InputBox.vue'
 
 export default {
   name: 'app',
@@ -19,6 +21,12 @@ export default {
   components: {
     Header,
     Chat,
+    InputBox,
   },
+  methods: {
+    send(event) {
+      alert(event.target.value)
+    }
+  }
 }
 </script>
---
+++ src/components/InputBox.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="inputbox">
+    <input class="inputbox__input" @keyup.enter="send" placeholder="Type a message... Hit Enter to Send"/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    send: { type: Function },
+  }
+}
+</script>
